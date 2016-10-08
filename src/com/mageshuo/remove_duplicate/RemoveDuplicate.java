bfa3f55e354d9017429348efb2e25e7daf7a0bca
jian.ma <jian.ma@dianrong.com>
2016-10-08 16:59:51 +0800

    Add array element.

---
+++ src/com/mageshuo/remove_duplicate/RemoveDuplicate.java
@@ -0,0 +1,20 @@
+package com.mageshuo.remove_duplicate;
+
+/**
+ * Created by majian on 08/10/2016.
+ * Please leave this header if you want to use the code.
+ */
+public class RemoveDuplicate {
+    public int removeDuplicates(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == nums[j]) {
+                continue;
+            }
+            nums[j] = nums[i - 1];
+            j++;
+        }
+        return j;
+    }
+    
+}
---
+++ src/com/mageshuo/remove_element/RemoveElement.java
@@ -0,0 +1,19 @@
+package com.mageshuo.remove_element;
+
+/**
+ * Created by majian on 08/10/2016.
+ * Please leave this header if you want to use the code.
+ */
+public class RemoveElement {
+    public int removeDuplicates(int[] nums) {
+        int j = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == nums[j]) {
+                continue;
+            }
+            j++;
+            nums[j] = nums[i];
+        }
+        return j + 1;
+    }
+}
