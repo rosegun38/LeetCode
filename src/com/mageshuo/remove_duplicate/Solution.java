a82638abd463966cb6c602b2dbef219382b32a2c
jian.ma <jian.ma@dianrong.com>
2016-10-08 17:02:17 +0800

    rename.

--- src/com/mageshuo/remove_duplicate/Solution.java
+++ src/com/mageshuo/remove_duplicate/Solution.java
@@ -4,7 +4,7 @@ package com.mageshuo.remove_duplicate;
  * Created by majian on 08/10/2016.
  * Please leave this header if you want to use the code.
  */
-public class RemoveDuplicate {
+public class Solution {
     public int removeDuplicates(int[] nums) {
         int j = 0;
         for (int i = 0; i < nums.length; i++) {
--- src/com/mageshuo/remove_element/Solution.java
+++ src/com/mageshuo/remove_element/Solution.java
@@ -4,7 +4,7 @@ package com.mageshuo.remove_element;
  * Created by majian on 08/10/2016.
  * Please leave this header if you want to use the code.
  */
-public class RemoveElement {
+public class Solution {
     public int removeDuplicates(int[] nums) {
         int j = 0;
         for(int i = 0; i < nums.length; i++) {
